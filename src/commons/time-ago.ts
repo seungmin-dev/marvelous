f306de1fc908bbbeeac5143d5e0c4a56fad96e7a
seungmin-dev <seungmin.dev@gmail.com>
2023-10-18 19:23:42 +0900

    Fix: time-ago 수정

--- src/commons/time-ago.ts
+++ src/commons/time-ago.ts
@@ -1,29 +1,29 @@
 export const timeAgo = (date: number) => {
   const seconds = Math.floor((new Date().getTime() - date) / 1000);
 
-  let interval = Math.floor(seconds / 31536000);
+  let interval = seconds / 31536000;
   if (interval > 1) {
-    return interval + " 년 전";
+    return Math.floor(interval) + " 년 전";
   }
 
-  interval = Math.floor(seconds / 2592000);
+  interval = seconds / 2592000;
   if (interval > 1) {
-    return interval + " 달 전";
+    return Math.floor(interval) + " 달 전";
   }
 
-  interval = Math.floor(seconds / 86400);
+  interval = seconds / 86400;
   if (interval > 1) {
-    return interval + " 일 전";
+    return Math.floor(interval) + " 일 전";
   }
 
-  interval = Math.floor(seconds / 3600);
+  interval = seconds / 3600;
   if (interval > 1) {
-    return interval + " 시간 전";
+    return Math.floor(interval) + " 시간 전";
   }
 
-  interval = Math.floor(seconds / 60);
+  interval = seconds / 60;
   if (interval > 1) {
-    return interval + " 분 전";
+    return Math.floor(interval) + " 분 전";
   }
 
   if (seconds < 10) return "방금 전";
