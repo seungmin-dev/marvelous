2e55d68d0702e026b68ee271c00065ab65873dad
seungmin-dev <seungmin.dev@gmail.com>
2023-10-20 19:54:02 +0900

    Fix: 북마크한 글 중 삭제된 글이 있을 시 오류 수정

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -6,18 +6,31 @@ import { useFetchBookmarks } from "../components/hooks/useFetchBookmarks";
 import { WrapperUI } from "../components/ui/wrapper";
 import { useFetchPostById } from "../components/hooks/useFetchPostById";
 import { BlankUI } from "../components/ui/blank";
+import { arrayRemove, doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase";
 
 export default function Bookmarks() {
   const [posts, setPosts] = useState<Post[]>([]);
   const { fetchBookmarks } = useFetchBookmarks();
   const { fetchPostById } = useFetchPostById();
+  const user = auth.currentUser;
 
   const getFetchPosts = async (bookmarks: string[]) => {
     const tempArr: Post[] = [];
     for (const i in bookmarks) {
-      await fetchPostById(bookmarks[i]).then((result: Post) =>
-        tempArr.push(result)
-      );
+      await fetchPostById(bookmarks[i]).then((result: Post) => {
+        // 북마크한 글 중에 삭제된 글이 있다면 북마크 글 아이디 목록에서 해당 아이디 삭제
+        if (result.createdAt === undefined) {
+          const userRef = doc(db, "users", user?.uid as string);
+          setDoc(
+            userRef,
+            {
+              bookmarks: arrayRemove(bookmarks[i]),
+            },
+            { merge: true }
+          );
+        } else tempArr.push(result);
+      });
     }
     setPosts(tempArr);
   };
